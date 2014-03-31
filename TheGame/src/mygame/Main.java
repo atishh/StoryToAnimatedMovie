4632ff226262674932bb1549817ca23e582bd788
atish <7705506+atishh@users.noreply.github.com>
2014-04-01 03:18:29 +0530

    Adding random trees and lake in project

--- TheGame/src/mygame/Main.java
+++ TheGame/src/mygame/Main.java
@@ -6,11 +6,16 @@ import com.jme3.light.DirectionalLight;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector2f;
 import com.jme3.math.Vector3f;
+import com.jme3.post.FilterPostProcessor;
 import com.jme3.renderer.RenderManager;
+import com.jme3.renderer.queue.RenderQueue;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
+import com.jme3.shadow.DirectionalLightShadowFilter;
+import com.jme3.shadow.DirectionalLightShadowRenderer;
 import com.jme3.terrain.Terrain;
 import com.jme3.terrain.geomipmap.TerrainLodControl;
+import com.jme3.water.SimpleWaterProcessor;
 import java.util.Random;
 
 /**
@@ -19,6 +24,8 @@ import java.util.Random;
  * @author normenhansen
  */
 public class Main extends SimpleApplication {
+
+    private Vector3f lightDir = new Vector3f(-0.39f, -0.32f, -0.74f);
 
     public static void main(String[] args) {
         Main app = new Main();
@@ -31,20 +38,23 @@ public class Main extends SimpleApplication {
         for (int i = 0; i < 400; i++) {
             float randomX = (float) (-100 + (rand.nextFloat() * ((100 + 100) + 1)));
             float randomZ = (float) (-100 + (rand.nextFloat() * ((100 + 100) + 1)));
-            if((randomX < 25) && (randomX > -25))
+            if ((randomX < 25) && (randomX > -25)) {
                 continue;
-            if((randomZ < 25) && (randomZ > -25))
+            }
+            if ((randomZ < 25) && (randomZ > -25)) {
                 continue;
+            }
             System.out.println("randomX = " + randomX);
             System.out.println("randomY = " + randomZ);
             float height = LakeTerrain.getHeight(new Vector2f(randomX, randomZ));
             System.out.println("height = " + height);
             Spatial tree = assetManager.loadModel("Scenes/Plants/Cylinder.001.mesh.j3o");
             //tree.setLodLevel(1);
-           // Geometry geo = (Geometry)tree.;
+            // Geometry geo = (Geometry)tree.;
 //Logger.getLogger("test").severe("AFTER XML LOAD num of lod : " + geo.getMesh().getNumLodLevels());
             //tree.getMesh().getNumLodLevels();
             tree.setLocalTranslation(randomX, height + 0.6f, randomZ);
+            tree.setShadowMode(RenderQueue.ShadowMode.CastAndReceive);
             //tree.setLocalTranslation(speed, speed, speed);
             //float randomRotate = (float) (-3.14f + (Math.random() * ((3.14f + 3.14f) + 1)));
 
@@ -67,13 +77,18 @@ public class Main extends SimpleApplication {
 
         Node rootNode = lake.getParent();
         Terrain LakeTerrain = (Terrain) rootNode.getChild("terrain-lake2");
+        rootNode.getChild("terrain-lake2").setShadowMode(RenderQueue.ShadowMode.CastAndReceive);
         //TerrainLodControl lodControl = new TerrainLodControl(LakeTerrain, getCamera());
-       // LakeTerrain.addControl(lodControl);
+        // LakeTerrain.addControl(lodControl);
         //LakeTerrain.
         TerrainLodControl lodControl = rootNode.getChild("terrain-lake2").getControl(TerrainLodControl.class);
-        if(lodControl != null)
+        if (lodControl != null) {
             lodControl.setCamera(getCamera());
+        }
         PlantTree(LakeTerrain);
+
+
+
 
         // if(LakeTerrain != NULL)
 
@@ -88,9 +103,39 @@ public class Main extends SimpleApplication {
          * A white, directional light source
          */
         DirectionalLight sun = new DirectionalLight();
-        sun.setDirection((new Vector3f(-0.5f, -0.5f, -0.5f)).normalizeLocal());
+        sun.setDirection((lightDir.normalizeLocal()));
         sun.setColor(ColorRGBA.White);
         rootNode.addLight(sun);
+
+        //Create water;
+        SimpleWaterProcessor waterProcessor =
+                new SimpleWaterProcessor(assetManager);
+        waterProcessor.setReflectionScene(rootNode);
+        waterProcessor.setLightPosition(lightDir.mult(-3000));
+        viewPort.addProcessor(waterProcessor);
+
+        Spatial waterPlane =
+                waterProcessor.createWaterGeometry(80, 80);
+        waterPlane.setMaterial(waterProcessor.getMaterial());
+        waterPlane.setLocalTranslation(-45, 1.5f, 65);
+        rootNode.attachChild(waterPlane);
+
+
+        //Shadow
+        DirectionalLightShadowRenderer dlsr =
+                new DirectionalLightShadowRenderer(assetManager, 1024, 2);
+        dlsr.setLight(sun);
+
+        //viewPort.addProcessor(dlsr);
+        DirectionalLightShadowFilter dlsf =
+                new DirectionalLightShadowFilter(assetManager, 1024, 2);
+        dlsf.setLight(sun);
+        dlsf.setEnabled(true);
+        FilterPostProcessor fpp = new FilterPostProcessor(assetManager);
+        fpp.addFilter(dlsf);
+        viewPort.addProcessor(fpp);
+        rootNode.setShadowMode(RenderQueue.ShadowMode.Off);
+        rootNode.setShadowMode(RenderQueue.ShadowMode.CastAndReceive);
 
         // viewPort.setBackgroundColor(ColorRGBA.White);
         // Default speed is too slow
